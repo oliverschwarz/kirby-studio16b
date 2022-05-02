a783e21a085e3bbc79c09085610479abd69d0a6b
Oliver Schwarz <oliver.schwarz@gmail.com>
2022-05-02 13:28:11 +0200

    Adding rough html construct to default template

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,19 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+
+  <meta charset="utf-8">
+  <title><?php echo $site->title()->html(); ?></title>
+  <meta name="viewport" content="width=device-width,minimum-scale=1.0">
+  <meta name="referrer" content="no-referrer">
+  <link rel="canonical" href="<?= $site->url() ?>">
+  <?php echo css('assets/layout.css'); ?>
+
+</head>
+<body>
+
 <h1><?= $page->title() ?></h1>
+
+
+</body>
+</html>
