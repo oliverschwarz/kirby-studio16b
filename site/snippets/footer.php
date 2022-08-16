74b6b7a088bb2c0c2d3a947e7c5cb7a36be3e966
Oliver Schwarz <oliver.schwarz@gmail.com>
2022-08-16 15:26:55 +0200

    Distributing the prototype section onto the different snippets and templates

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,43 +1,41 @@
-<footer class="cf">
+<footer>
 	<div class="stage">
+        <figure class="sticker">
+            <img src="<?php echo $site->url(false); ?>/assets/logo-mini.svg">
+        </figure>
+    </div>
+    <div class="direct-contact stage">
 
-		<div class="sticker">
-			<img src="/assets/logo-s16b.svg">
-		</div>
+        <address>
+                <strong>Studio16b</strong><br>
+                Weegener Str. 16b<br>
+                53797 Lohmar
+        </address>
 
-		<div class="direct-contact">
+        <p>
+            +49 1579 2347346<br>
+            <a href="mailto:hallo@studio16b.com">hallo@studio16b.com</a>
+        </p>
 
-			<address>
-					<strong>Studio16b</strong><br>
-					Weegener Str. 16b<br>
-					53797 Lohmar
-			</address>
+        <div class="social">
+            <h3>Folgen Sie uns auf LinkedIn</h3>
+            <ul>
+                <li class="linkedin"><a href="">Janina Pakusch</a></li>
+                <li class="linkedin"><a href="">Oliver Schwarz</a></li>
+            </ul>
+            <aside>
+                <?php echo $site->LinkedInText(); ?>
+            </aside>
+        </div>
 
-			<p>
-				+49 1579 2347346<br>
-				<a href="mailto:hallo@studio16b.com">hallo@studio16b.com</a>
-			</p>
+        <nav class="standards">
+            <ul>
+        <?php foreach ($site->find('info')->children()->listed() as $item): ?>
+                <li><?php echo $item->title()->link(); ?></li>
+        <?php endforeach; ?>
+            </ul>
+        </nav>
 
-			<div class="social">
-				<h3>Follow us</h3>
-				<ul>
-					<li class="linkedin"><a href="https://linkedin.com/in/janina.pakusch">Janina Pakusch</a></li>
-					<li class="linkedin"><a href="https://linkedin.com/in/-oschwarz">Oliver Schwarz</a></li>
-				</ul>
-			</div>
+    </div>
 
-			<nav class="site-nav">
-				<ul>
-		<?php foreach ($site->find('info')->children()->listed() as $item): ?>
-					<li><?php echo $item->title()->link(); ?></li>
-		<?php endforeach; ?>
-				</ul>
-			</nav>
-
-		</div>
-
-	</div>
 </footer>
-
-</body>
-</html>
--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,29 +1,19 @@
+<!-- header stage: logo, navigation -->
 <header class="home">
+
   <div class="stage">
 
-    <div id="logo">
-      <img src="/assets/logo-studio16b.svg">
-    </div>
+        <div id="logo">
+            <a href="<?php echo $site->url(false); ?>"><img src="<?php echo $site->url(false); ?>/assets/logo.svg"></a>
+        </div>
 
-    <nav class="main-nav">
-      <div class="stage">
-        <input class="menu-btn" type="checkbox" id="menu-btn" />
-        <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
-        <ul class="menu">
-        <?php foreach ($site->children()->listed() as $item): ?>
-          <li><?php echo $item->title()->link(); ?></li>
-        <?php endforeach; ?>
-        </ul>
-      </div>
-    </nav>
+        <nav id="nav-main">
+          <ul class="menu">
+          <?php foreach ($site->children()->listed() as $item): ?>
+            <li><?php echo $item->title()->link(); ?></li>
+          <?php endforeach; ?>
+          </ul>
+        </nav>
 
   </div>
-
-  <!-- Commitment -->
-  <div class="commitment">
-    <div class="stage">
-      <h1><?= $page->headline() ?></h1>
-    </div>
-  </div>
-
 </header>
