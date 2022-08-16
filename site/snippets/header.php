6f2fe2e226fc6a0a10d17cc02006245fdd5dc152
Oliver Schwarz <oliver.schwarz@gmail.com>
2022-08-16 09:59:17 +0200

    Checking in changes before switching to main

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,21 +1,9 @@
-<header class="home">
+<header>
   <div class="stage">
 
     <div id="logo">
       <img src="/assets/logo-studio16b.svg">
     </div>
-
-    <nav class="main-nav">
-      <div class="stage">
-        <input class="menu-btn" type="checkbox" id="menu-btn" />
-        <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
-        <ul class="menu">
-        <?php foreach ($site->children()->listed() as $item): ?>
-          <li><?php echo $item->title()->link(); ?></li>
-        <?php endforeach; ?>
-        </ul>
-      </div>
-    </nav>
 
   </div>
 
--- site/templates/home.php
+++ site/templates/home.php
@@ -1,37 +1,29 @@
 <?php snippet('meta'); ?>
 
-<header class="home">
-	<div class="stage">
+<header class="home cf">
 
-		<div id="logo">
-			<img src="/assets/logo-studio16b.svg">
-		</div>
-
+	<div id="logo">
+		<img src="/assets/logo-studio16b.svg" alt="Studio16b Logo">
 	</div>
 
-	<!-- Startpage home.php wallpaper -->
-	<figure class="wallpaper">
-			<img src="assets/resilient-wall-c.jpg" alt="An old partly painted wall with the bricks shining through in bright colors">
-	</figure>
-
-	<!-- Commitment -->
-	<div class="commitment">
-		<div class="stage">
-			<h1><?= $page->headline() ?></h1>
-		</div>
-	</div>
+	<nav id="site-nav">
+	  	<ul class="menu">
+			<?php foreach ($site->children()->listed() as $item): ?>
+				<li><?php echo $item->title()->link(); ?></li>
+			<?php endforeach; ?>
+	  	</ul>
+	</nav>
 
 </header>
 
 <main>
-	<div class="stage">
-		<h2 class="introduction">
-			<?= $page->intro()->kirbytext() ?>
-		</h2>
-		<div class="main-content">
-			<?= $page->text()->kirbytext() ?>
-		</div>
-	</div>
+
+    <figure class="wallpaper">
+        <img src="assets/resilient-wall-c.jpg" alt="An old partly painted wall with the bricks shining through in bright colors">
+    </figure>
+
+	<h1><?= $page->headline() ?></h1>
+
 </main>
 
 <?php snippet('footer'); ?>
