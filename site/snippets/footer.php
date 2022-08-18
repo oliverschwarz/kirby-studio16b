555d6a980fa5c6cac7bb1e4f1cea8749b6fbd3c3
Oliver Schwarz <oliver.schwarz@gmail.com>
2022-08-18 06:49:08 +0200

    Removed all the static content in the footer, replaced with CMS content - manageble over the Kirby panel

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -3,25 +3,24 @@
         <figure class="sticker">
             <img src="<?php echo $site->url(false); ?>/assets/logo-mini.svg">
         </figure>
+        <div id="direct-contact">
+            <address>
+                    <strong><?= $site->companyname() ?></strong><br>
+                    <?= $site->address()->kt() ?>
+            </address>
+            <p>
+                <?= $site->phone() ?><br>
+                <a href="mailto:<?= $site->email() ?>"><?= $site->email() ?></a>
+            </p>
+        </div>
     </div>
-    <div class="direct-contact stage">
-
-        <address>
-                <strong>Studio16b</strong><br>
-                Weegener Str. 16b<br>
-                53797 Lohmar
-        </address>
-
-        <p>
-            +49 1579 2347346<br>
-            <a href="mailto:hallo@studio16b.com">hallo@studio16b.com</a>
-        </p>
-
+    <div class="stage">
         <div class="social">
             <h3>Folgen Sie uns auf LinkedIn</h3>
             <ul>
-                <li class="linkedin"><a href="">Janina Pakusch</a></li>
-                <li class="linkedin"><a href="">Oliver Schwarz</a></li>
+            <?php foreach($site->find('about')->children()->listed() as $person): ?>
+                <li class="linkedin"><a href="<?= $person->linkedinlink() ?>"><?= $person->Title() ?></a></li>
+            <?php endforeach; ?>
             </ul>
             <aside>
                 <?php echo $site->LinkedInText(); ?>
