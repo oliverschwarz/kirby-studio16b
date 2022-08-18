3792110b26f6aecc6aef2134594abd445854e821
Oliver Schwarz <oliver.schwarz@gmail.com>
2022-08-18 09:59:58 +0200

    Adding opengraph implementation. Setting values in site.de.txt

--- site/snippets/meta.php
+++ site/snippets/meta.php
@@ -9,5 +9,14 @@
   <link rel="canonical" href="<?= $site->url() ?>">
   <?= css('assets/layout.css'); ?>
 
+  <link rel="shortcut icon" href="favicon.png">
+
+  <meta name="twitter:card" value="summary">
+  <meta property="og:title" content="<?= $site->ogtitle() ?>">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="<?= $site->url(false) ?>">
+  <meta property="og:image" content="<?= $site->find('home')->image()->url(); ?>">
+  <meta property="og:description" content="<?= $site->ogdescription() ?>">
+
 </head>
 <body>
