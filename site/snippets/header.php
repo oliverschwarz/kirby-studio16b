cbebc44adb1196ac7d69b24048500ccff04be3c3
Oliver Schwarz <oliver.schwarz@gmail.com>
2023-03-10 12:36:25 +0100

    Adding Home link to every page except Home and Error

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -9,12 +9,15 @@
 
         <nav id="nav-main">
           <ul class="menu">
+          <?php if (!$page->isHomeOrErrorPage()): ?>
+            <li><a href="<?= $site->url() ?>">Home</a></li>
+          <?php endif; ?>
           <?php foreach ($site->children()->listed() as $item): ?>
             <li<?php if($item->isActive()): ?> class="selected"<?php endif; ?>><?= $item->title()->link(); ?></li>
           <?php endforeach; ?>
           
           <?php foreach($kirby->languages() as $language): if ($kirby->language() != $language): ?>
-            <li class="language-icon <?= $language->code() ?>"><a href="<?= $language->url() ?>" hreflang="<?= $language->code() ?>"><img src="/assets/<?= $language->code() ?>.svg" width="16" height="16" alt="<?= $language->code() ?>"></a></li>
+            <li class="language-icon <?= $language->code() ?>"><a href="<?= $language->url() ?>" hreflang="<?= $language->code() ?>"><img src="/assets/<?= $language->code() ?>.svg" width="20" height="20" alt="<?= $language->code() ?>"></a></li>
           <?php endif; endforeach; ?>
 
           </ul>
